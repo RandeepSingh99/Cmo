dbbc68ce28086a3aeaf1a380a2a4ea8093fd96fa
Randeep Singh <81235647+RandeepSingh99@users.noreply.github.com>
2024-11-30 19:31:05 +0530

    added banner

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Header from '../../components/Header'
-
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Header from '../../components/Header';
+import banner from '../../../assets/images/banner.png';
+import {scaledValue} from '../../utils/designUtils';
 const Home = () => {
   return (
     <View>
-      <Header/>
+      <Header />
+      <Image
+        resizeMode="contain"
+        style={styles.banner}
+        source={banner}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  banner:{width: scaledValue(375), height: scaledValue(177)}
+});
