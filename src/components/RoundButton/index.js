69a36cc6a6f77674387807fe17d3c3e498e0d37a
Randeep Singh <81235647+RandeepSingh99@users.noreply.github.com>
2024-11-30 19:14:20 +0530

    design changes

--- src/components/RoundButton/index.js
+++ src/components/RoundButton/index.js
@@ -1,8 +1,8 @@
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-import { scaledValue } from '../../utils/designUtils';
+import {scaledValue} from '../../utils/designUtils';
 import liveImg from '../../../assets/images/liveImg.png';
-import { appColors } from '../../utils/constants/colors';
+import {appColors} from '../../utils/constants/colors';
 
 const RoundButton = () => {
   return (
@@ -16,9 +16,10 @@ export default RoundButton;
 
 const styles = StyleSheet.create({
   actionButtonIcons: {width: scaledValue(40), height: scaledValue(40)},
-  touchable:{
-    borderWidth:1,
-    borderRadius:100,
-    borderColor:appColors.gray,marginLeft:scaledValue(2)
-  }
+  touchable: {
+    borderWidth: 1,
+    borderRadius: 100,
+    borderColor: appColors.gray,
+    marginLeft: scaledValue(2),
+  },
 });
